9e9f5e52aa2f58aa5573a172e26ee18225ce0a43
David <51931592+dvdzmr@users.noreply.github.com>
2024-03-21 13:55:19 +0200

    Text is now censored and output as a new file

--- text_modifier.py
+++ text_modifier.py
@@ -1,28 +1,54 @@
 # This module fetches text from sources and returns arrays of formatted text.
-
+import json
 
 class TextModifier:
 
-    def __init__(self):
-        self.text = ""
-        self.censor_type = ""
+    def __init__(self, textfile, censor_type):
+        self.text = textfile  # filename of .txt file
+        self.censor_type = censor_type
+        self.source = ""
         self.result = []
+        self.json = ""
 
-    def get_text(self, filename):
-        pass
-        # Check if file exist
-        # Load textfile into an array with "." as delimiter
+    def get_text(self):
+        try:
+            with open(self.text, 'r') as file:
+                text = file.read()  # .split('.')
+                self.source = text
+        except FileNotFoundError:
+            print('File not found.')
 
-    def censor_text(self, censor_type, banned_words):
-        pass
-        # Censor text with provided censor_type
-        # eg: censor_type = censor (replace banned words with *)
-        # censor_type = positive (replace with positive words)
-        # Put results in self.result
+        # print(self.source)
 
-        # If censor_type is positive, load the alternative_words.json to iterate over later.
+    def censor_text(self, banned_words):
+        if self.censor_type == "direct":  # replace banned words with *
+            for word in banned_words:
+                if word in self.source:
+                    censor = len(word) * '*'
+                    self.source = self.source.replace(word, censor)
+
+        elif self.censor_type == "replace":  # replace banned words with another word
+            self.load_json()
+            for word in banned_words:
+                if word in self.source:
+                    try:
+                        replacement = self.json[0][word]
+                    except KeyError:  # incase key does not exist, we catch the error here
+                        censor = len(word) * '*'
+                        replacement = censor
+                    self.source = self.source.replace(word, replacement)
+        else:
+            print('Invalid censor type.')
+
+
+    def load_json(self):
+        with open("alternative_words.json", 'r') as file:
+            self.json = json.load(file)
+            file.close()
 
     def show_results(self):
-        pass
-        # Print results
-    
+        str_result = ''.join(self.source)
+        with open(self.text + "_edited", 'w') as file:
+            file.write(self.source)
+
+        print("Text modified successfully.")
